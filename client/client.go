a4e84d39c55959695a61e90986a727040c75f8e9
Syfaro <syfaro@foxpaw.in>
2015-09-23 16:04:53 -0500

    change client to use types package instead of duplicates, comments

--- client/client.go
+++ client/client.go
@@ -1,60 +1,19 @@
+// Package mcapi has methods for requesting information from mcapi.us
 package mcapi
 
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/syfaro/mcapi/types"
 	"io/ioutil"
 	"net/http"
 )
 
-const (
-	APIEndpoint = "https://mcapi.us"
-)
+// APIEndpoint is the endpoint to use for requesting any information.
+const APIEndpoint = "https://mcapi.us"
 
-type ServerStatusPlayers struct {
-	Max int `json:"max"`
-	Now int `json:"now"`
-}
-
-type ServerStatusServer struct {
-	Name     string `json:"name"`
-	Protocol int    `json:"protocol"`
-}
-
-type ServerStatus struct {
-	Status      string              `json:"status"`
-	Online      bool                `json:"online"`
-	Motd        string              `json:"motd"`
-	Error       string              `json:"error"`
-	Players     ServerStatusPlayers `json:"players"`
-	Server      ServerStatusServer  `json:"server"`
-	LastOnline  string              `json:"last_online"`
-	LastUpdated string              `json:"last_updated"`
-}
-
-type ServerQueryPlayers struct {
-	Max  int      `json:"max"`
-	Now  int      `json:"now"`
-	List []string `json:"list"`
-}
-
-type ServerQuery struct {
-	Status      string             `json:"status"`
-	Online      bool               `json:"online"`
-	Error       string             `json:"error"`
-	Motd        string             `json:"motd"`
-	Version     string             `json:"version"`
-	GameType    string             `json:"game_type"`
-	GameID      string             `json:"game_id"`
-	ServerMod   string             `json:"server_mod"`
-	Map         string             `json:"map"`
-	Players     ServerQueryPlayers `json:"players"`
-	Plugins     []string           `json:"plugins"`
-	LastOnline  string             `json:"last_online"`
-	LastUpdated string             `json:"last_updated"`
-}
-
-func GetServerStatus(ip string, port int) (*ServerStatus, error) {
+// GetServerStatus allows you to ping a server and get basic information.
+func GetServerStatus(ip string, port int) (*types.ServerStatus, error) {
 	resp, err := http.Get(fmt.Sprintf("%s/server/status?ip=%s&port=%d", APIEndpoint, ip, port))
 	if err != nil {
 		return nil, err
@@ -63,7 +22,7 @@ func GetServerStatus(ip string, port int) (*ServerStatus, error) {
 
 	data, err := ioutil.ReadAll(resp.Body)
 
-	var status ServerStatus
+	var status types.ServerStatus
 	err = json.Unmarshal(data, &status)
 	if err != nil {
 		return nil, err
@@ -72,7 +31,10 @@ func GetServerStatus(ip string, port int) (*ServerStatus, error) {
 	return &status, nil
 }
 
-func GetServerQuery(ip string, port int) (*ServerQuery, error) {
+// GetServerQuery allows you to query a server to get more detailed information.
+//
+// Note that this function requires you to have query enabled on your server.
+func GetServerQuery(ip string, port int) (*types.ServerQuery, error) {
 	resp, err := http.Get(fmt.Sprintf("%s/server/query?ip=%s&port=%d", APIEndpoint, ip, port))
 	if err != nil {
 		return nil, err
@@ -81,7 +43,7 @@ func GetServerQuery(ip string, port int) (*ServerQuery, error) {
 
 	data, err := ioutil.ReadAll(resp.Body)
 
-	var status ServerQuery
+	var status types.ServerQuery
 	err = json.Unmarshal(data, &status)
 	if err != nil {
 		return nil, err
