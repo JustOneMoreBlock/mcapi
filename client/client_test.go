46ae4b79f783e8b5e255f0973c375b5fc919dc9f
Syfaro <syfaro@foxpaw.in>
2015-09-23 16:24:25 -0500

    add examples to tests

--- client/client_test.go
+++ client/client_test.go
@@ -1,6 +1,7 @@
 package mcapi
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -20,6 +21,18 @@ func TestGetServerStatusBad(t *testing.T) {
 	}
 }
 
+func ExampleGetServerStatus() {
+	status, err := GetServerStatus("mc.syfaro.net", 25565)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("You have %d/%d players online!", status.Players.Now, status.Players.Max)
+
+	// Output:
+	// You have 0/20 players online!
+}
+
 func TestGetServerQueryGood(t *testing.T) {
 	_, err := GetServerQuery("mc.syfaro.net", 25565)
 	if err != nil {
@@ -34,4 +47,16 @@ func TestGetServerQueryBad(t *testing.T) {
 		t.Log("Bad request was made and no error was created")
 		t.Fail()
 	}
+}
+
+func ExampleGetServerQuery() {
+	status, err := GetServerQuery("mc.syfaro.net", 25565)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("You have %d/%d players on a server running %s!", status.Players.Now, status.Players.Max, status.ServerMod)
+
+	// Output:
+	// You have 0/20 players on a server running CraftBukkit on Bukkit 1.8.3-R0.1-SNAPSHOT!
 }
